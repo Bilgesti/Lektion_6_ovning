b91217cc354bae880632a61c9735096cd1840fe7
Bilge927 <bilegdemberel.bayasgalanbayar@stud.sti.se>
2023-10-02 14:03:10 +0200

    Breakpoint

--- src/com/bilgee/lektion_6_ovning/Main.java
+++ src/com/bilgee/lektion_6_ovning/Main.java
@@ -4,24 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Part3");
-        //Få appen att sluta krascha!
-        //Skapa en try catch runt for-loopen och if-satsen som skapade
-        //kraschen tidigare.
-        //(Detta ska kasta ett felmeddelande.
-        //Detta går att testa om du har en ‘SOUT’ efter ‘try’, körs den så
-        //fungerar det!)
-
-    try {
-        System.out.println("Crash");
         for (int i = 0; i < 5; i++) {
             if (i > 1) {
                 System.out.println(i + 1);
             }
         }
-    }catch (Exception error) {
-        System.out.println("End");
-    }
+
 
     }
 
