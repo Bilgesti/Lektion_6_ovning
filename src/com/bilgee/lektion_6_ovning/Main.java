017e529db50fbfdd085517324738ca76f893d6cb
Bilge927 <bilegdemberel.bayasgalanbayar@stud.sti.se>
2023-10-02 13:29:29 +0200

    Part1

---
+++ src/com/bilgee/lektion_6_ovning/Main.java
@@ -0,0 +1,21 @@
+package com.bilgee.lektion_6_ovning;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Part1");
+        // Lägg en ‘try catch’ som kollar om det går att dividera med 0!
+
+        try{
+            System.out.println(6/0);
+        }catch (Exception error) {
+
+        }
+
+
+
+    }
+
+}
+
