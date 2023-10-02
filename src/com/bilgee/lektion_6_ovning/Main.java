3a8499887ddd0b2206ba86d8ee038ec6c68102a3
Bilge927 <bilegdemberel.bayasgalanbayar@stud.sti.se>
2023-10-02 13:43:44 +0200

    Part3

--- src/com/bilgee/lektion_6_ovning/Main.java
+++ src/com/bilgee/lektion_6_ovning/Main.java
@@ -4,18 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Part2");
-        //Försök nu att:
-        //Krascha programmet med en for loop!
-        //Med en if-sats
-        //Med extra måsvingar, parenteser och andra symboler!
+        System.out.println("Part3");
+        //Få appen att sluta krascha!
+        //Skapa en try catch runt for-loopen och if-satsen som skapade
+        //kraschen tidigare.
+        //(Detta ska kasta ett felmeddelande.
+        //Detta går att testa om du har en ‘SOUT’ efter ‘try’, körs den så
+        //fungerar det!)
 
+    try {
+        System.out.println("Crash");
         for (int i = 0; i < 5; i++) {
-            if(i>1) {
-                System.out.println(i+1);
+            if (i > 1) {
+                System.out.println(i + 1);
             }
-        }}
-
+        }
+    }catch (Exception error) {
+        System.out.println("End");
+    }
 
     }
 
